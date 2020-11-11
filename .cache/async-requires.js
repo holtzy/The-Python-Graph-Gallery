dd83cb9f112bf1d09d29baa56ad638d40e7711af
holtzy <yan.holtz@datadoghq.com>
2020-11-11 11:59:59 +0100

    start scatterplot section

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -8,6 +8,7 @@ exports.components = {
   "component---src-pages-density-plot-js": () => import("./../../../src/pages/density-plot.js" /* webpackChunkName: "component---src-pages-density-plot-js" */),
   "component---src-pages-histogram-js": () => import("./../../../src/pages/histogram.js" /* webpackChunkName: "component---src-pages-histogram-js" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-scatter-plot-js": () => import("./../../../src/pages/scatter-plot.js" /* webpackChunkName: "component---src-pages-scatter-plot-js" */),
   "component---src-pages-violin-plot-js": () => import("./../../../src/pages/violin-plot.js" /* webpackChunkName: "component---src-pages-violin-plot-js" */),
   "component---src-templates-basic-js": () => import("./../../../src/templates/basic.js" /* webpackChunkName: "component---src-templates-basic-js" */)
 }
