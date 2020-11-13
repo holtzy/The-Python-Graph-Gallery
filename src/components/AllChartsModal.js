faaa27f6273e08bc85b67a71507eb2e1ae1c6cde
holtzy <yan.holtz@datadoghq.com>
2020-11-13 18:59:54 +0100

    start improving the all charts modal

--- src/components/AllChartsModal.js
+++ src/components/AllChartsModal.js
@@ -14,7 +14,7 @@ import { fullUrlToInternalLink } from "../util/utils";
 export default function AllChartsModal({ show, handleClose }) {
   const logoList = chartTypesInfo.map((chart, i) => {
     return (
-      <Col id={i} xs={2} md={2}>
+      <Col id={i} xs={1} md={1}>
         <Link to={fullUrlToInternalLink(chart.pythonURL)}>
           <SectionLogo chartType={chart.logo} />
         </Link>
@@ -28,13 +28,16 @@ export default function AllChartsModal({ show, handleClose }) {
       onHide={handleClose}
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      size="lg"
+      className="allFamiliesModal"
     >
-      <ModalDialog contentClassName="allFamiliesModal">
-        <Button className="closeModalButton" onClick={handleClose}>
-          X
-        </Button>
+      <Button className="closeModalButton" onClick={handleClose}>
+        X
+      </Button>
+      <Modal.Body >
+
         <Row>{logoList}</Row>
-      </ModalDialog>
+      </Modal.Body>
     </Modal>
   );
 }
