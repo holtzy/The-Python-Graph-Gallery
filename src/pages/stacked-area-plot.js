4711459ef53be51ca21f9740cddd9bc990b2286b
holtzy <yan.holtz@datadoghq.com>
2020-12-26 19:05:04 +0100

    stacked area section

--- src/pages/stacked-area-plot.js
+++ src/pages/stacked-area-plot.js
@@ -7,14 +7,15 @@ import Row from "react-bootstrap/Row";
 import ChartImageContainer from "../components/ChartImageContainer";
 import ChartFamilySection from "../components/ChartFamilySection";
 import { Link } from "gatsby";
-import { Matplotlib, Seaborn } from "../components/MiscellaneousLogos"
+import { Matplotlib } from "../components/MiscellaneousLogos"
 import { Col } from "react-bootstrap";
 import CodeChunk from "../components/CodeChunk"
 import ChartImage from "../components/ChartImage";
 import Spacing from "../components/Spacing";
+import { Button } from "react-bootstrap";
 
 const chartDescription =
-  "<p>A <a href='https://www.data-to-viz.com/graph/area.html'>stacked area chart</a> is displays the evolution of a numeric variable for several groups of a dataset. Each group is displayed on top of each other, making it easy to read the evolution of the total, but hard to read each group value accurately. In python, stacked area charts are mainly done thanks to the <code>stackplot()</code> function</p>";
+  "<p>A <a href='https://www.data-to-viz.com/graph/stackedarea.html'>stacked area chart</a> displays the evolution of a numeric variable for several groups of a dataset. Each group is displayed on top of each other, making it easy to read the evolution of the total, but hard to read each group value accurately. In python, stacked area charts are mainly done thanks to the <code>stackplot()</code> function</p>";
 
 const quickCode = `# library
 import numpy as np
@@ -22,11 +23,13 @@ import matplotlib.pyplot as plt
 
 # Create data
 x=range(1,6)
-y=[1,4,6,8,4]
+y1=[1,4,6,8,9]
+y2=[2,2,7,10,12]
+y3=[2,8,5,10,6]
 
-# Area plot
-plt.fill_between(x, y)
-plot.show()
+# Basic stacked area chart.
+plt.stackplot(x,y1, y2, y3, labels=['A','B','C'])
+plt.legend(loc='upper left')
 `
 
 export default function StackedAreaPlot() {
@@ -44,16 +47,16 @@ export default function StackedAreaPlot() {
         <h2 id="Quick">&#9201; Quick start</h2>
         <Row className="align-items-center">
           <Col md={6}>
-            <p>There are 2 main ways to build an area chart with Matplotlib. In both case it requires 2 numeric vectors of values as input.</p>
-            <ul>
-              <li>The <code>fill_between()</code> function</li>
-              <li>The <code>stackplot()</code> function that is more useful for <Link to='/stacked-area-plot'>stacked area</Link> charts</li>
-            </ul>
+            <p>Here is a quick start code snippet to demo how the <code>stackplot()</code> function of <code>matplotlib</code> works.</p>
+            <p>Note that here each groups are provided in its own vector of values. The <Link to='/250-basic-stacked-area-chart'>basic stacked area</Link> blog post
+            explains how to use the function from any type of data format.
+            </p>
+
           </Col>
           <Col md={6}>
-            <Link to={"/240-basic-area-chart"}>
-              <ChartImage imgName="240_basic_area_chart"
-                caption="The most basic area chart one can make with python and matplotlib" />
+            <Link to={"/250-basic-stacked-area-chart"}>
+              <ChartImage imgName="250_basic_stacked_area_chart"
+                caption="The most basic stacked area chart one can make with python and matplotlib" />
             </Link>
           </Col>
         </Row>
@@ -62,48 +65,96 @@ export default function StackedAreaPlot() {
 
       <Spacing />
 
+      <div className="greySection" id="warning">
+        <Container>
+          <h2 id="Warning">&#9888;&#65039; The issue with stacking</h2>
+          <p>
+            Stacked area charts must be used with care since they suffer a number of caveats. They are
+            appropriate to study the evolution of the whole and the relative proportions of each group, but
+            not to study the evolution of each individual group.
+          </p>
+          <p>
+            For instance, it is pretty hard to understand how the green group evolves on the chart below.
+            Can you spot if its value is increasing, decreasing or stable?
+          </p>
+          <a href='https://www.data-to-viz.com/caveat/stacking.html'>
+            <Button size="sm">Read more</Button>
+          </a>
+          <div style={{ maxWidth: "400px", margin: "0 auto", padding: 30 }}>
+            <a href='https://www.data-to-viz.com/caveat/stacking.html' >
+              <ChartImage imgName={'issue-with-stacking'} caption={'It is hard to see how the green group evolves.'} />
+            </a>
+          </div>
+        </Container>
+      </div>
+
+      <Spacing />
+
       <Container>
-        <h2 id="Matplotlib"><Matplotlib />Area chart with <code>Matplotlib</code></h2>
+        <h2 id="Matplotlib"><Matplotlib />Stacked Area chart with <code>Matplotlib</code></h2>
         <p>
-          <code>Matplotlib</code> is a great fit to build an area chart thanks to its <code>fill_between()</code> function. Here are a few examples explaining its basics
-          and how to apply some common customization.
+          <code>Matplotlib</code> is the most common way to build a stacked area chart with Python. The examples
+          below start by explaining to basics of the <code>stackplot()</code> function. The also describe the most common
+          type of customization like changing colors, controling group order and more.
         </p>
         <Row>
           <ChartImageContainer
-            imgName="240_basic_area_chart"
-            caption="The most basic area chart one can make with python and matplotlib"
-            linkTo="/240-basic-area-chart"
+            imgName="250_basic_stacked_area_chart"
+            caption="Most basic stacked area chart"
+            linkTo="/250-basic-stacked-area-chart"
           />
           <ChartImageContainer
-            imgName="241_custom_area_chart1"
-            caption="Control chart color (Matplotlib)"
-            linkTo="/241-improve-area-chart"
+            imgName="251_seaborn_style_on_stacked_area_chart"
+            caption="Apply seaborn style on the matplotlib stacked area chart"
+            linkTo="/251-stacked-area-chart-with-seaborn-style"
           />
           <ChartImageContainer
-            imgName="241_custom_area_chart2"
-            caption="Control line color"
-            linkTo="/241-improve-area-chart"
+            imgName="253_color_and_stacked_area_chart2"
+            caption="Control area colors"
+            linkTo="/253-control-the-color-in-stacked-area-chart"
+          />
+          <ChartImageContainer
+            imgName="242_area_chart_and_faceting"
+            caption="Area chart and small multiple"
+            linkTo="/242-area-chart-and-faceting"
           />
         </Row>
-        <div style={{ maxWidth: "600px", margin: "0 auto", padding: 30 }}>
-          <Link to='/243-area-chart-with-white-grid'>
-            <ChartImage imgName={'243_another_area_chart'} caption={'Area chart with custom grid'} />
-          </Link>
-        </div>
       </Container>
 
       <Spacing />
 
       <Container>
-        <h2 id="Seaborn"><Seaborn />Area chart with <code>Seaborn</code></h2>
+        <h2 id="Percent stacked"><Matplotlib />Percent Stacked Area chart with <code>Matplotlib</code></h2>
         <p>
-          <code>Seaborn</code> is another great alternative to build an area chart with <code>python</code>. The below examples
-          show how to start basic, apply usual customization, and use the small multiple technique
-          for when you have several groups to compare.
+          A variation of the stacked area graph is the percent stacked area graph where the value of
+          every groups are normalized at each time stamp. It allows to study the percentage of each
+          group in the whole more efficiently.
+        </p>
+        <p>
+          Fortunately, the <code>pandas</code> library has a <code>divide()</code> function that
+          allows to apply this normalization easily.
+        </p>
+        <Row>
+          <ChartImageContainer
+            imgName="255_percent_stacked_area_chart"
+            caption="Basic percent stacked area chart"
+            linkTo="/255-percentage-stacked-area-chart"
+          />
+        </Row>
+      </Container>
+
+      <Spacing />
+
+      <Container>
+        <h2 id="Pandas">Stacked Area chart with <code>Pandas</code></h2>
+        <p>
+          <code>Pandas</code> is mainly useful to normalize your dataset and build a stacked
+          area chart. Surprisingly, it also provides a <code>plot.area()</code>
+          that can be handy to build a stacked area chart.
         </p>
         <div style={{ maxWidth: "600px", margin: "0 auto", padding: 30 }}>
-          <Link to='/242-area-chart-and-faceting'>
-            <ChartImage imgName={'242_area_chart_and_faceting'} caption={'Area chart with small multiple, seaborn'} />
+          <Link to='/254-pandas-stacked-area-chart'>
+            <ChartImage imgName={'254_pandas_stacked_area_chart2'} caption={'Stacked area chart with Pandas'} />
           </Link>
         </div>
       </Container>
