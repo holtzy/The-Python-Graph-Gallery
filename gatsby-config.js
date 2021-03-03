edd17115262dc8eaab506a739dd65e0c48cbe646
holtzy <yan.holtz@datadoghq.com>
2021-03-03 09:38:21 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,5 +49,27 @@ module.exports = {
         icon: `static/logo/Home_single_big.png`
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "UA-79254642-2", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
   ],
 };
