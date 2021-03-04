eb0dc691a3d0c11841fa7412fad45ac84d737c1a
holtzy <yan.holtz@datadoghq.com>
2021-03-04 10:53:32 -0400

    gtag in header

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
           // Setting this parameter is also optional
           respectDNT: true,
         },
