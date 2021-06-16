671cdc41125ab4bf349dec699819164f7ccb8eb8
holtzy <yan.holtz@datadoghq.com>
2021-06-16 23:44:21 +0200

    remove the public folder to make gitignore not track it anymore

--- public/sponsor/script_reformat_img.sh
+++
@@ -1,5 +0,0 @@
-FILE=$1
-convert Screenshot*.png -resize 200x200 new.png
-convert -size 200x200 xc:white new.png  -gravity center -composite output.png
-mv output.png $FILE
-rm Screenshot*.png new.png
