77d408c9a7f2d580b5fc8daaebcbdfe0c265f5b9
holtzy <yan.holtz@datadoghq.com>
2023-06-23 15:26:09 +0200

    add ticks to insert adThrive

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Helmet } from "react-helmet-async";
 import PropTypes from "prop-types";
 
@@ -20,23 +20,6 @@ export default function Layout({
   seoDescription,
   keywords,
 }) {
-  // add Raptive ads
-  useEffect(() => {
-    // Code that depends on the `window` object
-    // This code will run after the component is mounted
-
-    // Example: Load an external script dynamically
-    const script = document.createElement("script");
-    script.src = "util/raptive.js";
-    script.async = true;
-    document.head.appendChild(script);
-
-    // Cleanup function
-    return () => {
-      // Code to clean up any resources if needed
-    };
-  }, []);
-
   return (
     <>
       <Helmet>
@@ -69,6 +52,26 @@ export default function Layout({
           name="twitter:image"
           content="https://github.com/holtzy/The-Python-Graph-Gallery/blob/master/static/overview_PGG.png?raw=true"
         />
+        <script>
+          {`(function (w, d) {
+            w.adthrive = w.adthrive || {};
+            w.adthrive.cmd = w.adthrive.cmd || [];
+            w.adthrive.plugin = "adthrive-ads-manual";
+            w.adthrive.host = "ads.adthrive.com";
+            var s = d.createElement("script");
+            s.async = true;
+            s.referrerpolicy = "no-referrer-when-downgrade";
+            s.src =
+              "https://" +
+              w.adthrive.host +
+              "/sites/6434366c7ccf1c58d32ab68f/ads.min.js?referrer=" +
+              w.encodeURIComponent(w.location.href) +
+              "&cb=" +
+              (Math.floor(Math.random() * 100) + 1);
+            var n = d.getElementsByTagName("script")[0];
+            n.parentNode.insertBefore(s, n);
+          })(window, document);`}
+        </script>
       </Helmet>
 
       <header>
