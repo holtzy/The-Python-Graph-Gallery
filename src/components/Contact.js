fdad28362ed5dd7de3f23f85286a2278ddbfae6f
Holtz Yan <holtzy@Holtzs-MacBook-Pro.local>
2023-07-18 16:41:41 +0200

    better contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,61 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./contact.css";
-import Button from "react-bootstrap/Button";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './contact.css';
+import Button from 'react-bootstrap/Button';
 
 // Component for the Contact & Edit section at the bottom of each page.
 // If a pageSlug prop is provided, a link to the according github page will be provided.
 export default function Contact({ pageSlug, isNotebook }) {
   const editPageUrl =
-    "https://github.com/holtzy/The-Python-Graph-Gallery/blob/master/src/notebooks/" +
+    'https://github.com/holtzy/The-Python-Graph-Gallery/blob/master/src/notebooks/' +
     pageSlug +
-    ".ipynb";
+    '.ipynb';
 
   return (
     <div className="contactSection lightSection">
       <h2 id="Edit this page" className="contactTitle">
-        {isNotebook ? "Contact & Edit" : "Contact"}
+        {isNotebook ? 'Contact & Edit' : 'Contact'}
       </h2>
+      <br />
       <p className="contactText">
-        👋 This document is a work by{" "}
-        <a href="https://www.yan-holtz.com">Yan Holtz</a>. Any feedback is
-        highly encouraged. You can{" "}
-        <a href="https://github.com/holtzy/The-Python-Graph-Gallery/issues">
-          fill an issue
-        </a>{" "}
-        on Github, drop me a message on
-        <a href="https://twitter.com/r_graph_gallery?lang=en">Twitter</a>, or
-        send an email pasting <code>yan.holtz.data</code> with{" "}
-        <code>gmail.com</code>.
-      </p>
-      <p>
-        Last but not least, you can{" "}
-        <a href="https://datavizuniverse.substack.com/">
-          subscribe to my newsletter
+        👋 This document is a work by{' '}
+        <a href="https://www.yan-holtz.com">Yan Holtz</a>. You can contribute on{' '}
+        <a
+          href="https://github.com/holtzy/The-Python-Graph-Gallery"
+          target="_blank"
+          rel="noreferrer"
+        >
+          github
         </a>
-        <br />
-        to know when some new tutorials are published! 🔥
+        , send me a feedback on{' '}
+        <a
+          href="https://twitter.com/R_Graph_Gallery"
+          target="_blank"
+          rel="noreferrer"
+        >
+          twitter
+        </a>{' '}
+        or subscribe to the{' '}
+        <a
+          href="https://datavizuniverse.substack.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          newsletter
+        </a>{' '}
+        to know when new examples are published! 🔥
       </p>
+
       {isNotebook && (
         <blockquote>
-          This page is just a <u>jupyter notebook</u>, you can edit it{" "}
+          This page is just a <u>jupyter notebook</u>, you can edit it{' '}
           <a href={editPageUrl}>here</a>. Please help me making this website
           better 🙏!
         </blockquote>
       )}
+
+      <br />
+
       <div>
         <Button
           size="md"
