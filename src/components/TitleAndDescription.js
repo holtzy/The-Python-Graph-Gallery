3363c511234e59b44ff9499a2bb1286029aa4272
Holtz Yan <holtzy@Holtzs-MacBook-Pro.local>
2023-07-18 15:30:47 +0200

    Allow prettier

--- src/components/TitleAndDescription.js
+++ src/components/TitleAndDescription.js
@@ -1,13 +1,12 @@
-import "./titleAndDescription.css";
-import React from "react";
+import './titleAndDescription.css';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-import Button from "react-bootstrap/Button";
-import { Link } from "gatsby";
-import { chartTypesInfo } from "../util/sectionDescriptions";
-import { fullUrlToInternalLink } from "../util/utils";
-import { Container } from "react-bootstrap";
-// import Sponsors from "../components/Sponsors";
+import Button from 'react-bootstrap/Button';
+import { Link } from 'gatsby';
+import { chartTypesInfo } from '../util/sectionDescriptions';
+import { fullUrlToInternalLink } from '../util/utils';
+import { Container } from 'react-bootstrap';
 
 // Note that the description received is a string that contains html.
 // It was not possible to pass JSX since it is sometimes passed directly from
@@ -20,23 +19,19 @@ export default function TitleAndDescription({ title, description, chartType }) {
       <div className="titleAndDescription">
         <h1 className="mainTitle">{title}</h1>
         <hr className="smallHr" />
-        {typeof description === 'string' 
-        ? <div
-        className="description"
-        dangerouslySetInnerHTML={{ __html: description }}
-      /> 
-        : <div
-        className="description"
-      >{description}</div>
-      }
+        {typeof description === 'string' ? (
+          <div
+            className="description"
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
+        ) : (
+          <div className="description">{description}</div>
+        )}
 
-
-        
-        
         {chartType && (
-          <div style={{ marginTop: "10px" }}>
+          <div style={{ marginTop: '10px' }}>
             <Link to={fullUrlToInternalLink(chartInfo.pythonURL)}>
-              <Button size="sm">{chartInfo.label + " section"}</Button>
+              <Button size="sm">{chartInfo.label + ' section'}</Button>
             </Link>
             <a href={chartInfo.dataToVizURL}>
               <Button size="sm">About this chart</Button>
@@ -50,9 +45,6 @@ export default function TitleAndDescription({ title, description, chartType }) {
 
 TitleAndDescription.propTypes = {
   title: PropTypes.string,
-  description: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element
-  ]),
-  chartType: PropTypes.string
+  description: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  chartType: PropTypes.string,
 };
