bafd17f75061f49f025cf49ec61ef21a193bcc91
Holtz Yan <holtzy@Holtzs-MacBook-Pro.local>
2023-07-18 11:34:22 +0200

    no ads in dev more

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,6 +7,9 @@ import TopNavbar from "../components/TopNavbar";
 import SideLogos from "../components/SideLogos";
 import TableOfContent from "../components/TableOfContent";
 
+// This is for ESLint, it does not understand that process is a global variable in a gatsby environment
+/*global process*/
+
 // Import Styles = bootstrap + custom
 import "../styles/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -53,26 +56,28 @@ export default function Layout({
           name="twitter:image"
           content="https://github.com/holtzy/The-Python-Graph-Gallery/blob/master/static/overview_PGG.png?raw=true"
         />
-        <script>
-          {`(function (w, d) {
-            w.adthrive = w.adthrive || {};
-            w.adthrive.cmd = w.adthrive.cmd || [];
-            w.adthrive.plugin = "adthrive-ads-manual";
-            w.adthrive.host = "ads.adthrive.com";
-            var s = d.createElement("script");
-            s.async = true;
-            s.referrerpolicy = "no-referrer-when-downgrade";
-            s.src =
-              "https://" +
-              w.adthrive.host +
-              "/sites/6434366c7ccf1c58d32ab68f/ads.min.js?referrer=" +
-              w.encodeURIComponent(w.location.href) +
-              "&cb=" +
-              (Math.floor(Math.random() * 100) + 1);
-            var n = d.getElementsByTagName("script")[0];
-            n.parentNode.insertBefore(s, n);
-          })(window, document);`}
-        </script>
+        {process.env.NODE_ENV === 'production' && 
+          <script>
+            {`(function (w, d) {
+              w.adthrive = w.adthrive || {};
+              w.adthrive.cmd = w.adthrive.cmd || [];
+              w.adthrive.plugin = "adthrive-ads-manual";
+              w.adthrive.host = "ads.adthrive.com";
+              var s = d.createElement("script");
+              s.async = true;
+              s.referrerpolicy = "no-referrer-when-downgrade";
+              s.src =
+                "https://" +
+                w.adthrive.host +
+                "/sites/6434366c7ccf1c58d32ab68f/ads.min.js?referrer=" +
+                w.encodeURIComponent(w.location.href) +
+                "&cb=" +
+                (Math.floor(Math.random() * 100) + 1);
+              var n = d.getElementsByTagName("script")[0];
+              n.parentNode.insertBefore(s, n);
+            })(window, document);`}
+          </script>
+        }
       </Helmet>
 
       <header>
