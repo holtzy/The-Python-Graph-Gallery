448ad1508bc056dacf4197e0857bd378af112549
Andru <andru@tinymighty.com>
2023-08-27 14:33:46 +0100

    Migrate jupyter notebook pages to use Gatsby filesystem routing api

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,46 +3,19 @@ const path = require(`path`);
 // This allows to render iframe
 // see https://github.com/gatsbyjs/gatsby/issues/17761
 // iframes are used to insert plotly outputs (interactive charts) to pages.
-const express = require('express')
+const express = require('express');
 exports.onCreateDevServer = ({ app }) => {
-  app.use(express.static('public'))
-}
-
-// This allows to transform every jupyter notebooks into pages.
-exports.createPages = async ({ graphql, actions: { createPage } }) => {
-  const blogPostTemplate = path.resolve(`src/templates/basic.js`);
-  const results = await graphql(
-    `
-      {
-        allJupyterNotebook {
-          nodes {
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    `
-  );
-  const posts = results.data.allJupyterNotebook.nodes;
-  posts.forEach((post) => {
-    createPage({
-      path: post.fields.slug,
-      component: blogPostTemplate,
-      context: {
-        slug: post.fields.slug,
-      },
-    });
-  });
+  app.use(express.static('public'));
 };
 
-exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions;
-  if (node.internal.type === "JupyterNotebook") {
-    createNodeField({
-      name: "slug",
-      node,
-      value: node.json.metadata.slug,
-    });
-  }
+// alias `slug` to `metadata.slug` for ease of use of `slug` in filesystem routing
+// i.e. {jupyterNotebook.slug}.js
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  const typeDefs = `
+    type JupyterNotebook implements Node {
+      slug: String @proxy(from: "metadata.slug")
+    }
+  `;
+  createTypes(typeDefs);
 };
