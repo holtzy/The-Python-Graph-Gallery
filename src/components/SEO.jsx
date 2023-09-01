676f9b388d12a7b74d52580b5d6e28b14e6b92de
Holtz Yan <holtzy@Holtzs-MacBook-Pro.local>
2023-09-01 09:20:26 +0200

    raptive script not using the gatsby Script API

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -7,26 +7,6 @@
 // AdThrive ads code is also added here
 
 import React from 'react';
-import { Script } from 'gatsby';
-
-function getAdThriveScriptUrl() {
-  if (typeof window === 'undefined') {
-    return '';
-  }
-  const w = window;
-  w.adthrive = w.adthrive || {};
-  w.adthrive.cmd = w.adthrive.cmd || [];
-  w.adthrive.plugin = 'adthrive-ads-manual';
-  w.adthrive.host = 'ads.adthrive.com';
-  return (
-    'https://' +
-    w.adthrive.host +
-    '/sites/6434366c7ccf1c58d32ab68f/ads.min.js?referrer=' +
-    w.encodeURIComponent(w.location.href) +
-    '&cb=' +
-    (Math.floor(Math.random() * 100) + 1)
-  );
-}
 
 export const SEO = ({ title, seoDescription, keywords }) => {
   return (
@@ -64,11 +44,27 @@ export const SEO = ({ title, seoDescription, keywords }) => {
         name="twitter:image"
         content="https://github.com/holtzy/The-Python-Graph-Gallery/blob/master/static/overview_PGG.png?raw=true"
       />
-      <Script
-        id="adthrive"
-        strategy="post-hydrate"
-        src={getAdThriveScriptUrl()}
-      />
+
+      <script>
+        {`(function (w, d) {
+               w.adthrive = w.adthrive || {};
+               w.adthrive.cmd = w.adthrive.cmd || [];
+               w.adthrive.plugin = "adthrive-ads-manual";
+               w.adthrive.host = "ads.adthrive.com";
+               var s = d.createElement("script");
+               s.async = true;
+               s.referrerpolicy = "no-referrer-when-downgrade";
+               s.src =
+                 "https://" +
+                 w.adthrive.host +
+                 "/sites/6434366c7ccf1c58d32ab68f/ads.min.js?referrer=" +
+                 w.encodeURIComponent(w.location.href) +
+                 "&cb=" +
+                 (Math.floor(Math.random() * 100) + 1);
+               var n = d.getElementsByTagName("script")[0];
+               n.parentNode.insertBefore(s, n);
+             })(window, document);`}
+      </script>
     </>
   );
 };
