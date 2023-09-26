9052d18ffbb280afefdf9718c412ee77637335e8
Holtz Yan <holtzy@Holtzs-MacBook-Pro.local>
2023-09-26 15:05:12 +0200

    load all fonts locally

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,5 +10,45 @@ export const onRenderBody = ({ setHeadComponents }) => {
       crossOrigin="anonymous"
       key="robotoRegular"
     />,
+    <link
+      rel="preload"
+      href="/fonts/roboto-v30-latin/roboto-v30-latin-italic.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="robotoItalic"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/roboto-v30-latin/roboto-v30-latin-700.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="roboto700"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/bitter-v33-latin/bitter-v33-latin-regular.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="bitterRegular"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/bitter-v33-latin/bitter-v33-latin-italic.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="bitterItalic"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/bitter-v33-latin/bitter-v33-latin-700.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="bitter700"
+    />,
   ]);
 };
