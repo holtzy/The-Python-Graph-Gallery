4f563891ce51abeebc374819f794c574a7c26d8c
Holtz Yan <yan.holtz.data@gmail.com>
2024-03-22 11:29:49 +0100

    fix year of copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
-import { Link } from "gatsby";
-import React from "react";
-import "./footer.css";
-import SocialMediaButtons from "./SocialMediaButtons";
+import { Link } from 'gatsby';
+import React from 'react';
+import './footer.css';
+import SocialMediaButtons from './SocialMediaButtons';
 
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="copyright">Copyright © the Python Graph Gallery 2023</div>
+      <div className="copyright">Copyright © the Python Graph Gallery 2024</div>
       <SocialMediaButtons />
       <div className="policy">
         <span>
