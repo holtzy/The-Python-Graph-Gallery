7c3b55ad74e6a69b61282c0e32c51136395331f8
Yan Holtz <yanholtz@yans-air.lan>
2024-04-29 14:50:47 +0200

    update path

--- src/pages/animation.js
+++ src/pages/animation.js
@@ -11,7 +11,7 @@ import { Button, Col } from 'react-bootstrap';
 import CodeChunk from '../components/CodeChunk';
 import Spacing from '../components/Spacing';
 import gapminderGif from '../../static/graph/animated_gapminder.gif';
-import outerSpaceGIF from '../../static/animations/web-animation-with-text8.gif';
+import outerSpaceGIF from '../../static/animations/web-animated-line-chart-with-annotation8.gif';
 import basicScatterGif from '../../static/graph/animated_chart.gif';
 import volcanoGif from '../../static/graph/animated_volcano.gif';
 import { SEO } from '../components/SEO';
@@ -107,12 +107,12 @@ export default function Correlogram() {
               written <b>through the animation</b>.
             </p>
             <br />
-            <Link to="/web-animation-with-text">
+            <Link to="/web-animated-line-chart-with-annotation">
               <Button size="sm">See code</Button>
             </Link>
           </Col>
           <Col md={8}>
-            <Link to={'/web-animation-with-text'}>
+            <Link to={'/web-animated-line-chart-with-annotation'}>
               <img
                 src={outerSpaceGIF}
                 alt="Animation with python"
