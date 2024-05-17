8b213cb43dfef5efb199e0ee9190112a92e7d08e
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-05-17 15:32:29 +0200

    improve color section

--- src/pages/python-colors.js
+++ src/pages/python-colors.js
@@ -14,7 +14,7 @@ import ChartImage from '../components/ChartImage';
 import { SEO } from '../components/SEO';
 
 const chartDescription =
-  '<p>A section about how to manage colors with Python. Coming soon.';
+  "<p><a href='https://python-graph-gallery.com/matplotlib'>Matplotlib</a> is a <strong>powerful plotting library</strong> in Python that offers <strong>extensive customization</strong> of plots, including a <strong>wide variety of colors</strong>.<br /><br />Colors in Matplotlib can be specified in numerous ways, such as by <em>name</em> (e.g., <code>red</code>, <code>blue</code>), by <em>hexadecimal code</em> (e.g., <code>#FF5733</code>), or by <em>RGB/RGBA tuples</em> (e.g., <code>(1, 0, 0)</code> for red). This <strong>flexibility</strong> makes it easy to create <strong>visually appealing</strong> and <strong>informative plots</strong>.<br /> <br />Additionally, <a href='https://python-graph-gallery.com/matplotlib'>matplotlib</a> supports a <strong>range of colormaps</strong>, which can be particularly useful for <strong>representing data with a gradient of values</strong>, enhancing the <strong>interpretability</strong> of complex datasets.</p>";
 
 const quickCode = `# libraries
 from mpl_toolkits.mplot3d import Axes3D
@@ -67,6 +67,9 @@ export default function Colors() {
                 </li>
               </ul>
             </p>
+            <Link to="/available-colors-in-matplotlib">
+              <Button size="sm">Learn more</Button>
+            </Link>
           </Col>
           <Col md={6}>
             <Link to={'/available-colors-in-matplotlib'}>
@@ -77,22 +80,100 @@ export default function Colors() {
             </Link>
           </Col>
         </Row>
-        <Link to="/available-colors-in-matplotlib">
-          <Button size="sm">Learn more</Button>
-        </Link>
       </Container>
 
       <Spacing />
 
       <Container>
-        <h2 id="Soon">
-          <Matplotlib />
-          Pre-made color palettes
-        </h2>
+        <h2>Pre-made categorical palettes</h2>
+        <Row>
+          <Col md={6}>
+            <p>
+              <a href="/matplotlib">Matplotlib</a> provides a set of pre-made
+              categorical palettes. These palettes are <b>ready to use</b> and
+              can be used to colorize your categorical data.
+            </p>
+            <p>
+              On this post, you will find the entire list of <b>available</b>{' '}
+              categorical palettes in <a href="/matplotlib">matplotlib</a>, how
+              to use them, and how to create your <b>own</b>.
+            </p>
+            <Link to="/available-categoric-palettes">
+              <Button size="sm">Learn more</Button>
+            </Link>
+          </Col>
+          <Col md={6}>
+            <Link to="/available-categoric-palettes">
+              <ChartImage
+                imgName="categorical_palettes"
+                caption="Available categorical color palettes in Matplotlib"
+              />
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+
+      <Spacing />
+
+      <Container>
+        <h2>Pre-made continuous palettes</h2>
         <p>
-          Matplotlib provides a set of pre-made color palettes that can be used
-          to colorize your charts. These palettes are available in the{' '}
+          <a href="/matplotlib">Matplotlib</a> provides a set of pre-made
+          continuous palettes that are split into 2 main categories:
+          <ul>
+            <li>
+              <b>Sequential:</b> Ideal for representing data that ranges from
+              low to high values.
+            </li>
+            <li>
+              <b>Diverging:</b> Ideal for representing data that ranges from
+              negative to positive values.
+            </li>
+          </ul>
         </p>
+        <Row>
+          <Col md={6}>
+            <br />
+            <p>
+              On this post, you will find the entire list of <b>available</b>{' '}
+              sequential palettes in <a href="/matplotlib">matplotlib</a>, how
+              to use them, and how to create your <b>own</b>.
+            </p>
+            <Link to="/available-continuous-palettes">
+              <Button size="sm">Sequential palettes</Button>
+            </Link>
+          </Col>
+          <Col md={6}>
+            <Link to="/available-continuous-palettes">
+              <ChartImage
+                imgName="sequential_palettes"
+                caption="Available sequential color palettes in Matplotlib"
+              />
+            </Link>
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col md={6}>
+            <Link to="/available-continuous-palettes">
+              <ChartImage
+                imgName="diverging_palettes"
+                caption="Available diverging color palettes in Matplotlib"
+              />
+            </Link>
+          </Col>
+          <Col md={6}>
+            <p>
+              Similar to the sequential palettes, you can find the entire list
+              of <b>available</b> diverging palettes in{' '}
+              <a href="/matplotlib">matplotlib</a>, how to use them, and how to
+              create your <b>own</b>.
+            </p>
+            <Link to="/available-continuous-palettes">
+              <Button size="sm">Diverging palettes</Button>
+            </Link>
+          </Col>
+        </Row>
       </Container>
 
       <Spacing />
