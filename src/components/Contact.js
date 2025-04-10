45a39f74b8f9d08f0e8352222044feeabe31be0d
Holtz Yan <yan.holtz@datadoghq.com>
2025-04-10 15:16:54 +0200

    data to viz instead of contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,58 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './contact.css';
-import SubscribeButton from './SubscribeButton';
+import SubscribeButtonCheatSheet from './SubscribeButtonCheatSheet';
 
-// Component for the Contact & Edit section at the bottom of each page.
-// If a pageSlug prop is provided, a link to the according github page will be provided.
-export default function Contact({ pageSlug, isNotebook }) {
-  const editPageUrl =
-    'https://github.com/holtzy/The-Python-Graph-Gallery/blob/master/src/notebooks/' +
-    pageSlug +
-    '.ipynb';
-
+export default function Contact() {
   return (
     <div className="contactSection lightSection">
       <h2 id="Edit this page" className="contactTitle">
-        {isNotebook ? 'Contact & Edit' : 'Contact'}
+        🚨 Grab the Data To Viz poster!
       </h2>
       <br />
       <p className="contactText">
-        👋 This document is a work by{' '}
-        <a href="https://www.yan-holtz.com">Yan Holtz</a>. You can contribute on{' '}
-        <a
-          href="https://github.com/holtzy/The-Python-Graph-Gallery"
-          target="_blank"
-          rel="noreferrer"
-        >
-          github
-        </a>
-        , send me a feedback on{' '}
-        <a
-          href="https://twitter.com/R_Graph_Gallery"
-          target="_blank"
-          rel="noreferrer"
-        >
-          twitter
-        </a>{' '}
-        or subscribe to the newsletter to know when new examples are published!
-        🔥
+        Do you know all the chart types? Do you know which one you should pick?
+        I made a <b>decision tree</b> that answers those questions. You can
+        download it for free!
       </p>
 
-      {isNotebook && (
-        <blockquote>
-          This page is just a <u>jupyter notebook</u>, you can edit it{' '}
-          <a href={editPageUrl}>here</a>. Please help me making this website
-          better 🙏!
-        </blockquote>
-      )}
+      <SubscribeButtonCheatSheet />
 
-      <SubscribeButton />
+      <img
+        src="https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/refs/heads/master/src/img/poster_small.png"
+        alt="handout Cheatsheet beginners"
+        width="100%"
+      />
     </div>
   );
 }
-
-Contact.propTypes = {
-  pageSlug: PropTypes.string,
-  isNotebook: PropTypes.boolean,
-};
